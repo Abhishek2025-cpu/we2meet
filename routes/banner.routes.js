d4637ebd4767cfe7b6d3b0e5ecd77fb845480365
mohdabdulla3 <mohdabdulla@pearlorganisation.com>
2026-09-14 14:05:41 +0530

    Fixed Create banner

--- routes/banner.routes.js
+++ routes/banner.routes.js
@@ -2,10 +2,12 @@ const express = require("express");
 const router = express.Router();
 
 const {
-  getPublicBanners
+  getPublicBanners,
+  getBannerByIdAdmin
 } = require("../controllers/banner.controller");
 
 router.get("/", getPublicBanners);
 router.get("/all", getPublicBanners);
+router.get("/:id", getBannerByIdAdmin);
 
 module.exports = router;
